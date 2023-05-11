f6e30bf1d74a0fe33ea860e900efcbbb7b86b69c
Ritik2004 <hritikgaur44@gmail.com>
2023-05-12 01:28:33 +0530

    this is a first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import CartItem from "./CartItem";
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello</h1>
+        <CartItem/>
+    </div>
+  );
+}
+
+export default App;
